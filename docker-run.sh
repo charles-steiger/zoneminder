1932db40d3a1ec39bbbd0a37b52f82128ee5c0a6
Charles Steiger <charles.steiger@protonmail.ch>
2020-06-26 18:16:51 -0500

    Customize docker run script

--- docker-run.sh
+++ docker-run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-docker run -d --name="Zoneminder" \
+docker run -d --name="zoneminder" \
 --net="bridge" \
 --privileged="true" \
 -p 8443:443/tcp \
 -p 9000:9000/tcp \
--e TZ="America/New_York" \
+-e TZ="America/Chicago" \
 -e SHMEM="50%" \
 -e PUID="99" \
 -e PGID="100" \
@@ -15,6 +15,6 @@ docker run -d --name="Zoneminder" \
 -e INSTALL_YOLO="0" \
 -e MULTI_PORT_START="0" \
 -e MULTI_PORT_END="0" \
--v "/mnt/cache/appdata/Zoneminder":"/config":rw \
--v "/mnt/cache/appdata/Zoneminder/data":"/var/cache/zoneminder":rw \
+-v "/home/docker/zoneminder/cache/appdata/zoneminder":"/config":rw \
+-v "/home/docker/zoneminder/cache/appdata/zoneminder/data":"/var/cache/zoneminder":rw \
 dlandon/zoneminder
